d59e3249a4f6362a46eca11cec6dbc2527d14926
Chiefette61 <>
2021-06-08 21:41:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let pluralNoun = "house"
+let name = "Mike";
+let verb = "eat";
+let adjective = "slow";
+let color = "pink";
+
+console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.");
+
+console.log ('\n')
+
+
+
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} — that allows ${name } to ${verb} with strings.`);
+name = "Jack";
+let currentAge = 9;
+
+console.log(`Next year, ${name} will be ${currentAge + 1}.`);
